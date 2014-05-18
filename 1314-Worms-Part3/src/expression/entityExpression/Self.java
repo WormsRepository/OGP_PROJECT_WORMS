eda362fda5b356a3a66b69e6d29ffc3638830990
Laurens <laurensloots@hotmail.com>
2014-05-19 00:33:03 +0200

    Simplified Self
    TODO:
    
    - make interface of variableAcces

--- 1314-Worms-Part3/src/expression/entityExpression/Self.java
+++ 1314-Worms-Part3/src/expression/entityExpression/Self.java
@@ -1,8 +1,5 @@
 package expression.entityExpression;
 
-import be.kuleuven.cs.som.annotate.Basic;
-import type.Entity;
-import worms.model.ImplementedPF;
 import worms.model.Worm;
 import expression.EntityExpression;
 
@@ -10,19 +7,6 @@ public class Self extends EntityExpression{
 
 	public Self(int line, int column, Worm worm) {
 		super(line, column);
-		this.worm = worm;
-	}
-	
-	@Basic
-	public Worm getWorm(){
-		return this.worm;
-	}
-	
-	private final Worm worm;
-
-	
-	@Override
-	public Entity getValue(){
-		return getWorm();
+		this.setEntity(worm);
 	}
 }
